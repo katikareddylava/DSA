c395adac64096571d5500c32e49c69582bdb8ca1
Lavakumar Katikareddy <lavakumar.katikareddy@corteva.com>
2024-06-16 10:57:05 +0530

    BS

---
+++ DSA/BinarySearch.cs
@@ -0,0 +1,76 @@
+﻿
+namespace DSA
+{
+    public class BinarySearch
+    {
+        public void CallBS()
+        {
+            Console.WriteLine("**************************Assume array should be in sorted order**************************");
+            Console.WriteLine("Please enter the size of the array: ");
+            string? input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input) && input.ToLower() != "quit" && input.ToLower() != "exit")
+            {
+                int size = Convert.ToInt32(input);
+                int[] arr = new int[size];
+                Console.WriteLine("Please enter the values in array: ");
+                for (int i = 0; i < size; i++)
+                {
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.WriteLine("Please enter the target: ");
+                int target = Convert.ToInt32(Console.ReadLine());
+
+                int index = BinarySearchMethod(arr, target);
+                Console.WriteLine("Target found at index: " + index);
+
+                Console.WriteLine("Please enter the size of the array to continue or please type EXIT or QUIT to close the program ");
+                input = Console.ReadLine();
+            }
+        }
+
+        public static int BinarySearchMethod(int[] arr, int target)
+        {
+            int s = 0;
+            int e = arr.Length - 1;
+            // find array is in ascending or descending;
+            bool isAsc = arr[s] < arr[e];
+            while (s <= e)
+            {
+                int m = s + (e - s / 2); // to bypass error of int max value for huge arr length;
+
+                if (arr[m] == target)
+                {
+                    return m;
+                }
+
+                if (isAsc)
+                {
+                    //Ascending order
+                    if (target < arr[m])
+                    {
+                        e = m - 1;
+                    }
+                    else
+                    {
+                        s = m + 1;
+                    }
+                }
+                else
+                {
+                    //Descending order
+                    if (target > arr[m])
+                    {
+                        e = m - 1;
+                    }
+                    else
+                    {
+                        s = m + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
--- DSA/Program.cs
+++ DSA/Program.cs
@@ -1,32 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please enter the target: ");
-int target = Convert.ToInt32(Console.ReadLine());
+﻿using DSA;
 
-int index = binarySearch(new int[] { 2, 4, 6, 8, 10, 12, 14, 20, 24, 25 }, target);
+BinarySearch bs=new();
+bs.CallBS();
 
-Console.WriteLine(index);
-Console.ReadKey();
-
-
-static int binarySearch(int[] arr, int target)
-{
-    int s = 0;
-    int e = arr.Length - 1;
-    while (s <= e)
-    {
-        int m = (s + e) / 2;
-        if (target < arr[m])
-        {
-            e = m - 1;
-        }
-        else if (target > arr[m])
-        {
-            s = m + 1;
-        }
-        else
-        {
-            return m;
-        }
-    }
-    return -1;
-}
